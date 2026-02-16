23bccc43cf4948a49879cf95180163e178636d8f
KELEE0810 <elieunlee@gmail.com>
2026-02-16 16:46:00 +0900

    Wireless connectivity by Pico W
    Handles connection attempts and status monitoring

---
+++ wifi_manger.py
@@ -0,0 +1,19 @@
+import network
+import time
+from secrets import secrets
+
+class WifiManager:
+    def __init__(self):
+        self.ssid = secrets['ssid']
+        self.pw = secrets['password']
+        self.wlan = network.WLAN(network.STA_IF)
+        
+    def connect(self):
+        self.wlan.active(True)
+        if not self.wlan.isconnected():
+            print(f"trying connecting Wifi...: {self.ssid}")
+            self.wlan.connect(self.ssid, self.pw)
+            for _ in range(10):
+                if self.wlan.isconnected(): break
+                time.sleep(1)
+        return self.wlan.isconnected()

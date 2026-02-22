703ceacf97dadf09a34245c62dd87a608b5998db
Eli Eun Lee <elieunlee@gmail.com>
2026-02-22 12:24:44 +0900

    Update secrets_template.py for multiple wifi

--- secrets_template.py
+++ secrets_template.py
@@ -1,5 +1,10 @@
 secrets = {
-    'networks': [
+    'networks': 
+    [
         {'ssid':'wifi no.1', 'pw':'password no.1'}, # 2.4G only
-        {'ssid':'wifi no.2', 'pw':'password no.2'}],
-    'google_url': 'your google_API_url'}
+        {'ssid':'wifi no.2', 'pw':'password no.2'},
+        #{'ssid':'wifi name', 'pw':'its password'},
+        # and you can add as many wifi as you want
+    ],
+    'google_url': 'your google_API_url'
+}
